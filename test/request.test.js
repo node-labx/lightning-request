29af4c1e58900423ab0974948af01b4f7924be42
kk <kk@youzan.com>
2020-06-02 11:19:28 +0800

    pretty code

--- test/request.test.js
+++ test/request.test.js
@@ -1,7 +1,7 @@
 const test = require('ava');
 const request = require('../index');
 
-test('参数 url 校验', async t => {
+test('参数 url 校验', async (t) => {
   try {
     await request();
   } catch (error) {
@@ -9,7 +9,7 @@ test('参数 url 校验', async t => {
   }
 });
 
-test('请求协议校验', async t => {
+test('请求协议校验', async (t) => {
   try {
     await request({
       url: 'ftp://api.github.com/repos/node-labx/lightning-request',
@@ -19,14 +19,14 @@ test('请求协议校验', async t => {
   }
 });
 
-test('HTTP to HTTPS', async t => {
+test('HTTP to HTTPS', async (t) => {
   const resp = await request({
     url: 'http://api.github.com/repos/node-labx/lightning-request',
   });
   t.is(resp.statusCode, 301);
 });
 
-test('简单 HTTP GET 请求 ', async t => {
+test('简单 HTTP GET 请求 ', async (t) => {
   const resp = await request({
     url: 'https://api.github.com/repos/node-labx/lightning-request',
   });
@@ -35,7 +35,7 @@ test('简单 HTTP GET 请求 ', async t => {
   t.is(resp.data.html_url, 'https://github.com/node-labx/lightning-request');
 });
 
-test('HTTP GET 请求带 Query 参数', async t => {
+test('HTTP GET 请求带 Query 参数', async (t) => {
   try {
     const resp = await request({
       url: 'https://api.github.com/search/repositories',
